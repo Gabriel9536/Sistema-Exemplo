81a7d00a2eab9e50f3383e8650ada86d311cef47
Gabriel9536 <gtome9170@gmail.com>
2026-08-03 12:54:04 -0300

    Primeira versão do sistema

---
+++ script.js
@@ -0,0 +1,17 @@
+const botao = document.getElementById("btnNovoSistema");
+//const botao2 = document.getElementById("btnFecharSistema");
+
+const mensagem = document.getElementById("mensagem");
+//const mensagem2 = document.getElementById("mensagem");
+
+botao.addEventListener("click", function () {
+
+    mensagem.textContent = "Novo Sistema Iniciado";
+
+});
+
+/*botao2.addEventListener("click", function () {
+
+    mensagem.textContent = "Sistema Fechado";
+
+});*/
